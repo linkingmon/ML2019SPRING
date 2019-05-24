735ae5fdde4e3f55f07ceba56f97e8d1f4cbb0a2
b05502145 <b05502145@ntu.edu.tw>
2019-05-24 11:10:18 +0800

    hw7

--- hw7/cluster.sh
+++ hw7/cluster.sh
@@ -1,1 +1,3 @@
+wget -O encoder.h5 'https://github.com/linkingmon/models/releases/download/0.0.2/encoder.h5'
+wget -O img.npy 'https://github.com/linkingmon/models/releases/download/0.0.2/img.npy'
 python3 cluster.py $1 $2 $3
--- hw7/pca.sh
+++ hw7/pca.sh
@@ -1,2 +1,5 @@
+wget -O S.npy 'https://github.com/linkingmon/models/releases/download/0.0.2/S.npy'
 wget -O U.npy 'https://github.com/linkingmon/models/releases/download/0.0.2/U.npy'
+wget -O V.npy 'https://github.com/linkingmon/models/releases/download/0.0.2/V.npy'
+wget -O mean_face.npy 'https://github.com/linkingmon/models/releases/download/0.0.2/mean_face.npy'
 python3 pca.py $1 $2 $3
